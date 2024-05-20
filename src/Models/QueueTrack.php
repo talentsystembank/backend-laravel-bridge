de8ffb096e8ed17a6e588da549c0d586fb1b8671
HungNA - Technical Manager <hungan@lisod.vn>
2024-05-20 11:09:05 +0700

    Add QueueTrack

---
+++ src/Models/QueueTrack.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\QueueTrack')) {
+    class QueueTrack extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\QueueTrack
+    {
+
+    }
+}
