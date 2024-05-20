c79c37432a0c6caf6f5f3e3abc3b4d6d71dc7ec0
HungNA - Technical Manager <hungan@lisod.vn>
2024-05-20 10:58:25 +0700

    Update at 2024-05-20 10:58:25 on HungNAs-MacBook-Pro

---
+++ src/Models/File.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\File')) {
+    class File extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\File
+    {
+
+    }
+}
