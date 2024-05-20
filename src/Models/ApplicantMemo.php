d986f5e9f5b747442a72d7163306a2775155603a
HungNA - Technical Manager <hungan@lisod.vn>
2024-05-20 11:22:42 +0700

    Add Models ApplicantMemo

---
+++ src/Models/ApplicantMemo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\ApplicantMemo')) {
+    class ApplicantMemo extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\ApplicantMemo
+    {
+
+    }
+}
