867b2b5d8f87241c35969ab73638194b584576dc
HungNA - Technical Manager <hungan@lisod.vn>
2024-05-20 10:55:43 +0700

    Update at 2024-05-20 10:55:43 on HungNAs-MacBook-Pro

---
+++ src/Models/CustomData.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\CustomData')) {
+    class CustomData extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\CustomData
+    {
+
+    }
+}
