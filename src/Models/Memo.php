894582f8c67cbc30ae9ba043b2271f8201fc96f5
HungNA - Technical Manager <hungan@lisod.vn>
2024-05-20 11:23:29 +0700

    Add Models Memo

---
+++ src/Models/Memo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\Memo')) {
+    class Memo extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\Memo
+    {
+
+    }
+}
