66fad6f009723070e56ed0b4c26057310a681590
HungNA - Technical Manager <hungan@lisod.vn>
2024-06-13 15:44:11 +0700

    Add Sources

---
+++ src/Models/Sources.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\Sources')) {
+    class Sources extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\Sources
+    {
+
+    }
+}
