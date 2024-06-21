502b914c2294be66c31c5ae2f8e670677b993106
HungNA - Technical Manager <hungan@lisod.vn>
2024-06-21 14:55:36 +0700

    Update user_activity

---
+++ src/Models/UserActivity.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\UserActivity')) {
+    class UserActivity extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\UserActivity
+    {
+    }
+}
