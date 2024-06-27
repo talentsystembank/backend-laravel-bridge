46d530c2685ca7af06a96271a38e12bfb0bfe562
HungNA - Technical Manager <hungan@lisod.vn>
2024-06-27 16:14:09 +0700

    Up

---
+++ src/Models/BasicDailyReports.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\BasicDailyReports')) {
+    class BasicDailyReports extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\BasicDailyReports
+    {
+
+    }
+}
