1729a47b96d1168a6a5f3943698ba5b733bd0e8f
HungNA - Technical Manager <hungan@lisod.vn>
2024-07-02 11:23:40 +0700

    Add CustomStatus

---
+++ src/Models/CustomStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\CustomStatus')) {
+    class CustomStatus extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\CustomStatus
+    {
+
+    }
+}
