3bababab09ba867d14d19a080ca4b3ff8f47cfa4
HungNA - Technical Manager <hungan@lisod.vn>
2024-07-05 14:19:30 +0700

    Add custom page

---
+++ src/Models/CustomPage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\CustomPage')) {
+    class CustomPage extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\CustomPage
+    {
+
+    }
+}
---
+++ src/Models/CustomPagesMeta.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\CustomPagesMeta')) {
+    class CustomPagesMeta extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\CustomPagesMeta
+    {
+
+    }
+}
