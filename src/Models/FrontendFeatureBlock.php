4dd34deec2201f93ad804f396cdd8efb2664ce04
HungNA - Technical Manager <hungan@lisod.vn>
2024-07-08 14:14:08 +0700

    Update feedback

---
+++ src/Models/FrontendFeatureBlock.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\FrontendFeatureBlock')) {
+    class FrontendFeatureBlock extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\FrontendFeatureBlock
+    {
+
+    }
+}
