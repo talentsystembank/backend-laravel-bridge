eb32564d326d31dbdc79e45ab929a2c8ab1842cf
HungNA <hungan@lisod.vn>
2024-07-24 11:12:04 +0700

    add ApiThirdPartyCallHistories

---
+++ src/Models/ApiThirdPartyCallHistories.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+if ( ! class_exists('App\Models\ApiThirdPartyCallHistories')) {
+    class ApiThirdPartyCallHistories extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Models\ApiThirdPartyCallHistories
+    {
+
+    }
+}
---
+++ src/Repositories/ApiThirdPartyCallHistoriesRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Project backend-laravel-bridge
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 07/06/2024
+ * Time: 09:23
+ */
+
+namespace App\Repositories;
+
+if (!class_exists('App\Repositories\ApiThirdPartyCallHistoriesRepository')) {
+    class ApiThirdPartyCallHistoriesRepository extends \nguyenanhung\AllTalentBank\Backend\MasterCoreService\Repository\ApiThirdPartyCallHistoriesRepository
+    {
+
+    }
+}
